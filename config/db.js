480dfd7ad77fd7013eee1381ce99c08454c98db2
Tanvir Ahmed <tanvir.bd.global@gmail.com>
2024-05-04 08:52:42 +0600

    added dc mongo uri

--- config/db.js
+++ config/db.js
@@ -2,8 +2,9 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
   try {
-    const conn = await mongoose.connect("mongodb+srv://tiktok:tiktok@tiktok.hg4nf29.mongodb.net/?retryWrites=true&w=majority&appName=TikTok");
+    const conn = await mongoose.connect(process.env.MONGO_URI);
     console.log(`MongoDB connected: ${conn.connection.host}`);
+    console.log(`MongoDB connected uri: ${process.env.MONGO_URI}`);
   } catch (error) {
     console.log(error);
   }
